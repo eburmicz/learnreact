b832352718836d4a5216dc1cb38ff5182550b488
ed <eburmicz@gmail.com>
2020-06-17 01:28:32 +0100

    added button, state and onclick function wth no real interactivity

--- react/src/App.js
+++ react/src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Components/Person';
 
-function App() {
-  return (
+class App extends Component {
+  // Create state as js objects
+  state = {persons:  [
+                      { name: 'Max', age: 30 },
+                      { name: 'Ed', age: 24 },
+                      { name: 'George', age: 60 }, 
+                    ]
+
+           }
+
+           switchNameHandler = () => {
+             console.log('switchNameHandler triggered')
+           }
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and asdasd to reload.
         </p>
-        <Person name='abc' age='12'/>
+        <button onClick={this.switchNameHandler} >Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name='abasasc' age='15'/>
         <Person name='abgggc' age='13'> <p>This is a child html</p> </Person>
         <a
@@ -24,7 +39,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+    };
 }
 
 export default App;
